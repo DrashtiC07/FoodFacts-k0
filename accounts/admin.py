db9357bff523ffb06e3bb0e6cd555d89865247e7
DrashtiC07 <drashtic0777@gmail.com>
2025-09-13 14:54:24 +0530

    final

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,16 +2,22 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, ProductReview, FavoriteProduct, DietaryGoal
 
-@admin.register(CustomUser)
+
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
-    search_fields = ('username', 'first_name', 'last_name', 'email')
-    ordering = ('username',)
-    
+    model = CustomUser
+
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+
     fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('phone_number', 'date_of_birth', 'profile_picture')}),
+        (None, {"fields": ("phone_number", "date_of_birth","profile_picture")}),
     )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("phone_number", "date_of_birth", "profile_picture")}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 @admin.register(ProductReview)
 class ProductReviewAdmin(admin.ModelAdmin):
--- accounts/models.py
+++ accounts/models.py
@@ -9,6 +9,7 @@ class CustomUser(AbstractUser):
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    age = models.PositiveIntegerField(null=True,blank=True)
     
     def __str__(self):
         return self.username
